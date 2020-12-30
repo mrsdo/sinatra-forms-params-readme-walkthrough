d7e1cd18f3e9672ce56741d13cc611e8f6a0cdca
Martinique Dolce <me@martidolce.com>
2020-12-30 09:15:27 -0500

    Spec Passing

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
+# Class App Routes
 class App < Sinatra::Base
-
   get '/food_form' do
     erb :food_form
   end
 
   # Add your post route and action below
-
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
 end
